d5447bc47e6a599fc14c8d81199f6b65b30df1cf
Saad <saadnajm.msc@gmail.com>
2024-05-08 10:29:15 +0100

    Initial commit

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!");
+﻿void myFirstMethod()
+{
+
+}
+void mySecondMethod()
+{
+
+}
+void myThirdMethod()
+{
+
+}
